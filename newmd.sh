fa5479eaaeb331490fc36f95166aabeb4aa78838
comdeng <comdeng@live.com>
2018-09-08 00:41:50 +0800

    增加一个添加新页面的命令

---
+++ newmd.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ $# -lt 2 ];then
+    echo "usage: ./newmd.sh [name] [title]"
+    exit 1
+fi
+
+name=$1
+title=$2
+
+touch src/$name.md
+
+cat > src/$name.md <<MARKDOWN
+---
+name: "$title"
+sitename: "Caddy中文文档"
+---
+
+# $title
+
+MARKDOWN
+
+echo "create markdown file src/$name.md finished."
+exit 0
